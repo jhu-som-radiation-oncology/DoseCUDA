e54be008e1842029072493dca5b48eb96d2bc5b0
Calin Reamy <nilac16@ymail.com>
2025-11-25 13:19:15 -0500

    Allow NumPy versions > 2

--- DoseCUDA/dose_kernels/dosemodule.cu
+++ DoseCUDA/dose_kernels/dosemodule.cu
@@ -1,7 +1,7 @@
 #include <algorithm>
 
-#define PY_SSIZE_T_CLEAN
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+//#define PY_SSIZE_T_CLEAN
+//#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <Python.h>
 #include <object.h>
 #include <numpy/arrayobject.h>
